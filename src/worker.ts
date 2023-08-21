2e5e6413fa72b71d7139360e1162b7f08eb15694
GhuniNew <aakanun43@gmail.com>
2023-08-21 13:02:08 +0700

    up

--- src/worker.ts
+++ src/worker.ts
@@ -1,51 +1,41 @@
-import { Hono } from 'hono';
+import { ErrorHandler, Hono } from 'hono';
 import { cors } from 'hono/cors';
-import {
-	TaskList,
-	CreateTask,
-	TaskOne,
-	updateTask,
-	deleteTask,
-	ListDatabase,
-	DeleteDatabase,
-	getStatus,
-	init,
-} from './api/data';
+import { TaskList, CreateTask, TaskOne, updateTask, deleteTask, ListDatabase, DeleteDatabase, getStatus, init } from './api/data';
 
 interface Env {
 	DB: any;
-	c: any;
 }
 
 const app = new Hono();
-app.use(cors({
-    origin: '*', // CORS
-}));
+app.use(
+	cors({
+		origin: '*', // CORS
+	})
+);
 
 //appp route for worker.ts
-app.get('/api', async  c => ListDatabase(c));
-app.get('/api/', async c => ListDatabase(c));
-app.get('/api/:name_db', async c => TaskList(c));
-app.get('/api/:name_db/:post_id', async c => TaskOne(c));
-app.post('/api/:name_db/p', async c => CreateTask(c));
-app.put('/api/:name_db/:id', async c => updateTask(c));
-app.delete('/api/:name_db/:post_id', async c => deleteTask(c));
-app.post('/api/:name_db', async c => init(c));
-app.delete('/api/:name', async c => DeleteDatabase(c));
-app.get('/status',async () => getStatus());
+app.get('/api', async (c) => ListDatabase(c));
+app.get('/api/', async (c) => ListDatabase(c));
+app.get('/api/:name_db', async (c) => TaskList(c));
+app.get('/api/:name_db/:post_id', async (c) => TaskOne(c));
+app.post('/api/:name_db/p', async (c) => CreateTask(c));
+app.put('/api/:name_db/:id', async (c) => updateTask(c));
+app.delete('/api/:name_db/:post_id', async (c) => deleteTask(c));
+app.post('/api/:name_db', async (c) => init(c));
+app.delete('/api/:name', async (c) => DeleteDatabase(c));
+app.get('/status', async () => getStatus());
 app.get('/', async () => getStatus());
 
-app.notFound((error: any) => {
-		return new Response(JSON.stringify({
-			error: error,
-			message: 'Not Found 404',
-		 }), { status: 404 });
+app.notFound(err => {
+	return new Response(JSON.stringify({
+		status: err.runtime,
+		error: err.req,
+		massage: err
+	}), { status: 404 });
 });
+
 app.onError((error: Error) => {
-		return new Response(JSON.stringify({
-			 error: error.message,
-			 message: 'Something went wrong code 500',
-			}), { status: 500 });
+	return new Response(( `error ${error.message}` ), { status: 500 });
 });
 
 export default app;
